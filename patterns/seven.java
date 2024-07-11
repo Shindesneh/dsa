b3a34ee3a179292253466e90ebf464a95c9afdd8
Shindesneh <snehaashinde04@gmail.com>
2024-07-11 23:26:37 +0530

    arrays

---
+++ patterns/seven.java
@@ -0,0 +1,49 @@
+class seven{
+    static void patternup(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+             }
+    }
+    static void patterndown(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<2*n-(2*i+1);j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+    }
+    System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        int n=6;
+        patternup(n);
+        patterndown(n);
+    }
+}
+
+//output
+//     *     
+//    ***
+//   *****
+//  *******
+// *********
+//***********
+//***********
+// *********
+//  *******
+//   *****
+//    ***
+//     *
