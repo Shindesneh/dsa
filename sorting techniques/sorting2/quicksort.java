d884485eaa77c6bd6630684796d6696c1ff6d891
Shindesneh <snehaashinde04@gmail.com>
2024-07-20 20:43:26 +0530

    sorting completed

--- sorting techniques/sorting2/quicksort.java
+++ sorting techniques/sorting2/quicksort.java
@@ -1,63 +1,29 @@
-
-
-
 import java.util.*;
-
 public class quicksort {
     static int partition(List<Integer> arr, int low, int high) {
-        int pivot = arr.get(low);
-        int i = low;
-        int j = high;
-
-        while (i < j) {
-            while (arr.get(i) <= pivot && i <= high - 1) {
+        int pivot = arr.get(high);//pvot be the last element
+        int i=(low-1);//i is index of smaller element
+        for(int j=low;j<high;j++){//j ranges from low to high
+            if(arr.get(j)<=pivot){
                 i++;
-            }
-
-            while (arr.get(j) > pivot && j >= low + 1) {
-                j--;
-            }
-            if (i < j) {
-                int temp = arr.get(i);
-                arr.set(i, arr.get(j));
-                arr.set(j, temp);
+                Collections.swap(arr,i,j);//swap i and j
             }
         }
-        int temp = arr.get(low);
-        arr.set(low, arr.get(j));
-        arr.set(j, temp);
-        return j;
+        Collections.swap(arr,i+1,high);//swap (i+1) and high
+        return i+1;
+    }
+    static void quicksort(List<Integer> arr,int low,int high){
+if(low<high){
+    int pi=partition(arr,low,high);//pi-partitioning index
+quicksort(arr,low,pi-1);
+quicksort(arr,pi+1,high);
+}
     }
 
-    static void qs(List<Integer> arr, int low, int high) {
-        if (low < high) {
-            int pIndex = partition(arr, low, high);
-            qs(arr, low, pIndex - 1);
-            qs(arr, pIndex + 1, high);
-        }
-    }
-
-    public static List<Integer> quickSort1(List<Integer> arr) {
-        // Write your code here.
-        qs(arr, 0, arr.size() - 1);
-        return arr;
-    }
-
-
-    public static void main(String args[]) {
-        List<Integer> arr = new ArrayList<>();
-        arr = Arrays.asList(new Integer[]{4, 6, 2, 5, 7, 9, 1, 3});
-        int n = arr.size();
-        System.out.println("Before Using insertion Sort: ");
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr.get(i) + " ");
-        }
-        System.out.println();
-        arr = quicksort.quickSort1(arr);
-        System.out.println("After insertion sort: ");
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr.get(i) + " ");
-        }
-        System.out.println();
+    public static void main(String[] args) {
+        List<Integer> arr=new ArrayList<>(Arrays.asList(7,8,9,1,10,5));
+        System.out.println("list before sorting "+arr);
+        quicksort(arr,0,arr.size()-1);
+        System.out.println("list after sorting "+arr);
     }
 }
