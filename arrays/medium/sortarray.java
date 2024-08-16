02bba223c258b815bb87cb8300f62ac3ab65e490
Shindesneh <snehaashinde04@gmail.com>
2024-08-16 23:50:24 +0530

    second commit

--- arrays/medium/sortarray.java
+++ arrays/medium/sortarray.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class sortarray {
+import java.util.*;
+public class sortarray {
+    static void sortarr(ArrayList<Integer> arr,int n){
+        int low=0,mid=0,high=n-1;
+while(mid<=high){
+    if(arr.get(mid)==0){
+        int temp=arr.get(low);
+        arr.set(low,arr.get(mid));
+        arr.set(mid,temp);
+        low++;
+        mid++;
+    }
+    else if(arr.get(mid)==1){
+        mid++;
+        }
+else{
+        int temp=arr.get(mid);
+        arr.set(mid,arr.get(high));
+        arr.set(high,temp);
+        high--;
+    }
+    }
+
+}
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arr=new ArrayList<>(Arrays.asList(new Integer[]{0,2,3,1,4,2,5}));
+        int n=7;
+        sortarr(arr,n);
+        System.out.println("after sorting");
+        for(int i=0;i<n;i++){
+            System.out.print(arr.get(i) + " ");
+        }
+        System.out.println();
+
+    }
+
 }
--- arrays/medium/subsetsun.java
+++ arrays/medium/subsetsun.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class subsetsun {
+import java.util.*;
+public class subsetsun {
+
+        public static void main(String[] args) {
+            int[] arr= {2, 5, 6, 3, 9};
+            int target = 100;
+            int n = arr.length;
+            boolean subset =sub(arr,target);
+if(subset){
+    System.out.println("the subset sum is found");
 }
+else{
+    System.out.println("the subset sum is  not found");
+
+}
+        }
+
+    static boolean sub(int[] arr,int target){
+        int n=arr.length;
+        for(int i=0;i<n;i++) {
+            for (int j = i + 1; j < n; j++) {
+                if ((arr[i] + arr[j]) == target) {
+                    System.out.println("yes");
+                    return true;
+                }
+            }
+        }
+
+                return false;
+            }
+        }
+
+
+
+
