27ed943fbdd4310693e736c040cfd7a5e7cbb6d6
Shindesneh <snehaashinde04@gmail.com>
2024-08-21 22:31:38 +0530

    fourth commit

--- arrays/easy/longestsubarray.java
+++ arrays/easy/longestsubarray.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class longestsubarray {
+import java.util.*;
+public class longestsubarray {
+    public static int longsubarray(int[] a ,long k) {
+        int n = a.length;
+        int left=0;int right=0;
+        long sum=a[0];
+        int maxlen=0;
+        while(right<n){
+            while(left<=right && sum>k){
+                sum-=a[left];
+                left++;
+            }
+if(sum==k){
+    maxlen=Math.max(maxlen,right-left+1);
 }
+right++;
+if(right<n)sum+=a[right];
+
+            }
+        return maxlen;
+        }
+
+    public static void main(String[] args) {
+        int[] a={2,3,5,1,9};
+        long k=10;
+        int len=longsubarray(a,k);
+        System.out.println("the length of subarray is: "+len);
+    }
+    }
+
