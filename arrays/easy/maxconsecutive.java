556bc9dc207a3e6007b0ff505df6f672e6d1ebc5
Shindesneh <snehaashinde04@gmail.com>
2024-08-23 23:57:45 +0530

    fourth commit

--- arrays/easy/maxconsecutive.java
+++ arrays/easy/maxconsecutive.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class maxconsecutive {
+import java.util.*;
+public class maxconsecutive {
+    static int maxconsecutive(int arr[] ){
+        int max=0;
+        int count=0;
+    for (int i=0;i<arr.length;i++){
+        if(arr[i]==1){
+            count++;
+        }
+        else{
+            count=0;
+        }
+        max=Math.max(max,count);
+    }
+return max;
+}
+
+    public static void main(String[] args) {
+        int arr[]={1,1,0,0,1,1,1,1};
+
+        int ans=maxconsecutive(arr);
+        System.out.println("the maximum consecutive integers are "+ans);
+    }
 }
