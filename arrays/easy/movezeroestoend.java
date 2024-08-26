b6377833ced86ad158bb040d6203801f8906e462
Shindesneh <snehaashinde04@gmail.com>
2024-08-26 22:17:58 +0530

    fourth commit

--- arrays/easy/movezeroestoend.java
+++ arrays/easy/movezeroestoend.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class movezeroestoend {
+import java.util.*;
+public class movezeroestoend {
+    static int[] movezeroes(int n,int[] arr) {
+        int j = -1;
+        for( int i = 0;i < n;i++){
+            if(arr[i]==0){
+                j=i;
+                break;
+            }
+        }
+        if(j==-1) return arr;
+        for(int i=j+1;i<n;i++){
+            if(arr[i]!=0){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                j++;
+            }
+
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={1,2,0,3,0,4,5};
+        int n=arr.length;
+        int[] ans=movezeroes(n,arr);
+        for(int i=0;i<n;i++){
+            System.out.print("  "+arr[i]);
+
+        }
+            }
 }
