595d0598207c0e9820bd538c1945c0af582f6825
Shindesneh <snehaashinde04@gmail.com>
2024-08-27 21:36:55 +0530

    fourth commit

--- arrays/easy/removeduplicates.java
+++ arrays/easy/removeduplicates.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class removeduplicates {
+public class removeduplicates {
+    static int removeduplicate(int arr[],int n){
+        int i=0;
+        for(int j=0;j< arr.length;j++){
+            if(arr[i]!=arr[j]){
+                i++;
+                arr[i]=arr[j];
+            }
+
+        }
+        return i+1;
+    }
+
+    public static void main(String[] args) {
+        int arr[]={1,1,2,2,3,3};
+        int n=6;
+        int k=removeduplicate(arr,n);
+        System.out.println("array after removal of duplicates is");
+        for(int i=0;i<k;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
 }
