dc45c47c66b06c0daf965e6213666c0ea9d261ac
Shindesneh <snehaashinde04@gmail.com>
2024-09-05 23:34:32 +0530

    fourth commit

--- arrays/easy/singlelement.java
+++ arrays/easy/singlelement.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class singlelement {
+import java.util.*;
+public class singlelement {
+    static int singleelement(int[] arr,int n){
+        int xor=0;
+        for(int i=0;i<n;i++){
+            xor=xor^arr[i];
+        }
+        return xor;
+    }
+
+    public static void main(String[] args) {
+        int arr[]={1,2,2,3,3};
+        int n=arr.length;
+        int a=singleelement(arr,n);
+        System.out.println("the single element in the array is "+a);
+    }
+
 }
